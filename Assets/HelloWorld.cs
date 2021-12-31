63ec21bb6790379f4a5cbe25591f143633394ff3
talessampaio-kazoo <tales.sampaio@kazoogames.com>
2021-12-31 16:08:55 -0300

    Create HelloWorld script

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public static class HelloWorld
+{
+  public static void Print()
+  {
+    Debug.Log("\n======================================================\n\n\n\tHello World!\n\n\n======================================================\n\n\n");
+  }
+}
