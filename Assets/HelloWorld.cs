69429a4cafd3bb733f6632c6c1aa41ed9c3c245d
talessampaio-kazoo <tales.sampaio@kazoogames.com>
2022-01-04 18:38:56 -0300

    Create Build Methods

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 public static class HelloWorld
@@ -5,5 +6,25 @@ public static class HelloWorld
   public static void Print()
   {
     Debug.Log("\n======================================================\n\n\n\tHello World!\n\n\n======================================================\n\n\n");
+  }
+  
+  public static void BuildAPK()
+  {
+      BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+      buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity"};
+      buildPlayerOptions.locationPathName = "Assets/Builds/Android";
+      buildPlayerOptions.target = BuildTarget.Android;
+      buildPlayerOptions.options = BuildOptions.None;
+      BuildPipeline.BuildPlayer(buildPlayerOptions);
+  }
+  
+  public static void BuildIOS()
+  {
+      BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+      buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity"};
+      buildPlayerOptions.locationPathName = "Assets/Builds/iOS";
+      buildPlayerOptions.target = BuildTarget.iOS;
+      buildPlayerOptions.options = BuildOptions.None;
+      BuildPipeline.BuildPlayer(buildPlayerOptions);
   }
 }
